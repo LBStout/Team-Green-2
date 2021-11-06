33ec82473b205a2eba13af595a95599f454b0728
Canyn777 <canyn.black@okstate.edu>
2021-11-05 21:42:07 -0500

    Agent detects when it has reached the waypoint

--- Assets/AgentControl.cs
+++ Assets/AgentControl.cs
@@ -21,5 +21,10 @@ public class AgentControl : MonoBehaviour
         //Vector3 targetPoint = new Vector3(-360.9f, 0.0f, 140.0f);
         Vector3 targetPoint = waypoint.transform.position;
         agent.SetDestination(targetPoint);
+
+        if (Vector3.Distance(targetPoint, transform.position) < 2.0f)
+        {
+            print("Waypoint reached");
+        }
     }
 }
