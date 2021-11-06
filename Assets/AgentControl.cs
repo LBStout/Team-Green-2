261059c71584974882c6737f1ddd603cfd6033ee
Canyn777 <canyn.black@okstate.edu>
2021-11-05 20:56:42 -0500

    Single agent travels to single waypoint

--- Assets/AgentControl.cs
+++ Assets/AgentControl.cs
@@ -6,18 +6,20 @@ using UnityEngine.AI;
 public class AgentControl : MonoBehaviour
 {
     public NavMeshAgent agent;
+    public GameObject waypoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello World");
+        waypoint = GameObject.Find("Waypoint");
     }
 
     // Update is called once per frame
     void Update()
     {
         //Vector3 targetPoint = new Vector3(-300.9f, 0.0f, 127.0f);
-        Vector3 targetPoint = new Vector3(-360.9f, 0.0f, 140.0f);
+        //Vector3 targetPoint = new Vector3(-360.9f, 0.0f, 140.0f);
+        Vector3 targetPoint = waypoint.transform.position;
         agent.SetDestination(targetPoint);
     }
 }
