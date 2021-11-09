d0c9e3391eff57e11178baaa550a7002072d6799
Joshuarox100 <joshua.bender@okstate.edu>
2021-11-09 17:43:16 -0600

    Created traffic lights.

---
+++ Assets/Scripts/Agents/StoplightManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoplightManager : MonoBehaviour
+{
+    [Tooltip("Length of a green light in seconds.")]
+    public float stateInterval = 10f;
+    private float timer;
+
+    [Tooltip("Stoplights at this intersection.")]
+    public StoplightClient[] stoplights;
+    [SerializeField, Tooltip("The index of the stoplight that is currently green in the list.")]
+    private int activeLight = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = stateInterval;
+        // Start the default active light as green.
+        stoplights[activeLight].CompleteChange();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0f)
+        {
+            UpdateLights();
+            timer = stateInterval;
+        }
+    }
+
+    // Turns the active light red and the next light green.
+    void UpdateLights()
+    {
+        stoplights[activeLight].StartChange();
+        if (stoplights.Length > 1)
+        {
+            // Move to the next element in the list or wrap back to the start.
+            activeLight = (activeLight + 1) % stoplights.Length;
+            stoplights[activeLight].StartChange();
+        }
+    }
+}
