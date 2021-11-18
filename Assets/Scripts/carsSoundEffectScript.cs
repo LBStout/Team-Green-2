140f12c2c1fc7bf047ae81d4e70451abbace0f4d
Dingyi <dkang@okstate.edu>
2021-11-18 12:16:54 -0600

    commented sound effect script of car

--- Assets/Scripts/carsSoundEffectScript.cs
+++ Assets/Scripts/carsSoundEffectScript.cs
@@ -12,6 +12,7 @@ public class carsSoundEffectScript : MonoBehaviour
 
     void Start()
     {
+        //play the sound when start
         runningSound.Play();
     }
 
@@ -20,12 +21,17 @@ public class carsSoundEffectScript : MonoBehaviour
     void Update()
     {
 
-
+        //get reference to the speed of the car
         float speed = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
 
+        //blend the sounds which is associated the speed of the car
+        //get the percentage speed of the car which is relative to the max speed of car
         float blend = Mathf.Abs(speed / (5f * 0.8f));
+        //assign corresponding pitch to the driving sounds of the car
         runningSound.pitch = Mathf.Lerp(minPitch, maxPitch, Mathf.Clamp(blend, 0, 1));
 
+
+        //below is the old code which is not used anymore
         //if (speed < minPitch)
         //{
         //    runningSound.pitch = minPitch;
