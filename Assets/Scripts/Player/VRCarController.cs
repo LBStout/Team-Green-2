70da549ead37e44f9a30e13b0144f3573cbbf9e0
Dingyi <dkang@okstate.edu>
2021-11-18 12:42:09 -0600

    commented the VRCarController script

--- Assets/Scripts/Player/VRCarController.cs
+++ Assets/Scripts/Player/VRCarController.cs
@@ -7,18 +7,21 @@ using UnityEngine.InputSystem;
 
 public class VRCarController : MonoBehaviour
 {
+    //get reference to steering wheel object
     public SteeringWheel wheel;
 
+    //get reference to sound sources
     public AudioSource runningSound;
     public AudioSource brakingSound;
 
+    //get reference to controll sources
     public InputActionAsset actions;
     public InputActionReference triggerRight;
     public InputActionReference triggerRightClick;
     public InputActionReference triggerLeft;
     public InputActionReference triggerLeftClick;
 
-    //input variables
+    //properties of car driving
     private float steerAngle;
     private bool isReverse;
 
@@ -48,25 +51,27 @@ public class VRCarController : MonoBehaviour
 
     private void Start()
     {
+        //enable inputs from XR controller
         actions.Enable();
     }
 
     private void FixedUpdate()
     {
-
         HandleMotor();
         HandleSteering();
         UpdateWheels();
     }
 
+    //used to switch the driving state of car -- forward or backward
     public void OnReverse()
     {
         isReverse = !isReverse;
     }
 
+    //used to handle the driving force and sounds effect of the car
     private void HandleMotor()
     {
-        //get value from input
+        //get value from XR controller input
         float brake = 0f;
         if (triggerLeftClick.action.ReadValue<float>() == 1f)
             brake = 1f;
@@ -78,53 +83,54 @@ public class VRCarController : MonoBehaviour
         else
             accelerate = triggerRight.action.ReadValue<float>();
 
+        //get value fo the speed of the car
         float speed = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+
         //change properties based on the input
         //if brake is not pressed
         if (brake == 0)
         {
+            //if the driving state is backward
             if (isReverse)
             {
                 accelerate *= -1;
             }
+            //if the speed exceed the max speed, used cannot accelerate more
             if (gameObject.GetComponent<Rigidbody>().velocity.magnitude > maxVelocity)
                 accelerate = 0;
 
+            //assign motor force to the front two wheels collider of the car
             frontLeftWheelCollider.motorTorque = accelerate * motorForce;
             frontRightWheelCollider.motorTorque = accelerate * motorForce;
 
+            //get relative speed of the car to the max speed
             float blend = Mathf.Abs(speed / (maxVelocity * 0.8f));
+            //update the pitch of the sounds of driving which is assoicate with the relative speed of the car
             runningSound.pitch = Mathf.Lerp(minPitch, maxPitch, Mathf.Clamp(blend, 0, 1));
 
-            //if (speed < minPitch) {
-            //    runningSound.pitch = minPitch;
-            //}
-            //else if (speed > maxPitch)
-            //{
-            //    runningSound.pitch = maxPitch;
-            //}
-            //else
-            //{
-            //    runningSound.pitch = speed;
-            //}
+
             //if this is the first time to run the car
             if (!isRunning)
             {
+                //update the state and start to play the sound of driving
                 isRunning = true;
                 runningSound.Play();
             }
+            //update the braking state when car is running
             isBraking = false;
 
+            //if there is no moving force for the wheel and the speed is low enough, car stop
             if (accelerate == 0 && speed < 1.67625f * (1f / 5))
                 gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
+        //if the brake is pressed
         else {
             //stop running and accelarating
             isRunning = false;
-            //if this is the first time call braking
-            //If speed is at half or more of max speed, play a long brake noise
+            //if this is the first time call braking and If speed is at half or more of max speed, play a long brake noise
             if (!isBraking && speed >= 8)
             {
+                //the time property indicates when in sound clip that it begins to play
                 brakingSound.time = 1f;
                 brakingSound.Play();
                 runningSound.pitch = minPitch;
@@ -142,33 +148,41 @@ public class VRCarController : MonoBehaviour
                 isBraking = true;
             }
 
+            //if speed is low enough, stop the car
             if (speed < 1.67625f * (3.5f / 5))
                 gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         }
 
+        //assign the drag friction to the car
         gameObject.GetComponent<Rigidbody>().drag = 
             Mathf.Lerp(maxDrag, minDrag, Mathf.Clamp(speed / (maxVelocity * 0.25f), 0f, 1f));
 
+        //update the bake force based on the input
         brakeForce = brake * 4000f;
+        //assign the brake force to the four wheel colliders
         frontLeftWheelCollider.brakeTorque = brakeForce;
         frontRightWheelCollider.brakeTorque = brakeForce;
         rearLeftWheelCollider.brakeTorque = brakeForce;
         rearRightWheelCollider.brakeTorque = brakeForce;
     }
 
+    //this function is used to assign the steer angle to the two front wheel colliders
     private void HandleSteering() {
+        //get relative value of the steer angle of steering wheel
         steerAngle = -maxSteerAngle * (wheel.Angle / wheel.angleLimit);
+        //assign the relative steer angle to the two front wheel colliders
         frontLeftWheelCollider.steerAngle = steerAngle;
         frontRightWheelCollider.steerAngle = steerAngle;
-
+        //get reference to the object
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        //assign the drag for turning
         rb.angularDrag = 0.5f + Mathf.Lerp(0, 1.5f, rb.velocity.magnitude / maxVelocity);
     }
 
 
+    //this function is used to call functions to update the position of four wheel of the car
     private void UpdateWheels()
     {
-
         UpdateWheelPos(frontLeftWheelCollider, frontLeftWheeelTransform);
         UpdateWheelPos(frontRightWheelCollider, frontRightWheeelTransform);
         UpdateWheelPos(rearLeftWheelCollider, rearLeftWheeelTransform);
@@ -176,7 +190,7 @@ public class VRCarController : MonoBehaviour
 
     }
 
-
+    //this function is used to change the position/orientation of a wheel object
     private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans) {
         Vector3 pos;
         Quaternion rot;
