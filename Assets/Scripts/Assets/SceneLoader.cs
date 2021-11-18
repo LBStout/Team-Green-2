01bd6d5896a27c9922e8b9595d833d5b1526b765
jobende-OSU <joshua.bender@okstate.edu>
2021-11-17 18:05:09 -0600

    Multiple Updates
    - All scenes will now be loaded when the game is built or run through unity
    - By clicking both trackpads, the player will be teleported into the car
    - Changed the way the car's sound pitch is calculated
    - Decreased the volume of the brake noise
    - Deleted unused Portal related things
    
    Co-Authored-By: Tenlor <67766241+Tenlor@users.noreply.github.com>

--- Assets/Scripts/Assets/SceneLoader.cs
+++ Assets/Scripts/Assets/SceneLoader.cs
@@ -12,7 +12,7 @@ public class SceneLoader : MonoBehaviour
     void Start()
     {
 
-            for (int i = 1; i <= Config.Scenes.Length; i++)
+            for (int i = 1; i < Config.Scenes.Length; i++)
             {
                 SceneManager.LoadScene(Config.Scenes[i].path, LoadSceneMode.Additive);
             }
