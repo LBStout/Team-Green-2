684bc713e9ed840f78a3477a1745d28599ec7837
Tenlor <Loganstout2013@gmail.com>
2021-11-18 18:20:08 -0600

    Commented TeleportToSeat Script

--- Assets/Scripts/TeleportToSeat.cs
+++ Assets/Scripts/TeleportToSeat.cs
@@ -16,6 +16,7 @@ public class TeleportToSeat : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Get the controls
         if(asset != null)
             asset.Enable();
     }
@@ -23,14 +24,18 @@ public class TeleportToSeat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Once the buttons are both pressed simultaneously, teleport the player 
         if (leftHand.action.ReadValue<float>() == 1 && rightHand.action.ReadValue<float>() == 1)
             RecenterPlayerPosition();
     }
 
     void RecenterPlayerPosition()
     {
+        //Get the distance from the seat to the player...
         disToSeat.x = transform.position.x - player.transform.position.x;
         disToSeat.z = transform.position.z - player.transform.position.z;
+        //And then add that to the player's position, teleporting them to the correct position
         player.transform.parent.transform.position += new Vector3(disToSeat.x, 0f, disToSeat.z);
     }
 }
